9fb604b26e6e056e5ee5d3c1c5e9b751a4a85c63
Le Luong Truong <truong.luong@sparkxlab.com>
2018-04-04 16:33:38 +0700

    Change the if statement for feedback

--- app/forms/developers_search_form.rb
+++ app/forms/developers_search_form.rb
@@ -8,15 +8,13 @@ class DevelopersSearchForm
   attribute :programming_language, String
 
   def search
-    if !email.present? && !language.present? && !programming_language.present?
-      Developer.none
-    else
-      query = Developer.includes(:developer_languages, :languages, :developer_programming_languages, :programming_languages)
-      query = query.where('developers.email like :email_search_string', email_search_string: "%#{sanitize_sql_like(email)}%") if email.present?
-      query = query.where(languages: { code: language }) if language.present?
-      query = query.where(programming_languages: { name: programming_language }) if programming_language.present?
-      query = query.order('developers.created_at desc')
-      query
-    end
+    return Developer.none if !email.present? && !language.present? && !programming_language.present?
+
+    query = Developer.includes(:developer_languages, :languages, :developer_programming_languages, :programming_languages)
+    query = query.where('developers.email like :email_search_string', email_search_string: "%#{sanitize_sql_like(email)}%") if email.present?
+    query = query.where(languages: { code: language }) if language.present?
+    query = query.where(programming_languages: { name: programming_language }) if programming_language.present?
+    query = query.order('developers.created_at desc')
+    query
   end
 end
