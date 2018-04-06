2a2a8e17eb236923d880f7459a78456b02957e6c
Le Luong Truong <truong.luong@sparkxlab.com>
2018-04-06 16:00:20 +0700

    Remove unneed include

--- app/forms/developers_search_form.rb
+++ app/forms/developers_search_form.rb
@@ -8,7 +8,7 @@ class DevelopersSearchForm
   def search
     return Developer.none if [language, programming_language].all?(&:blank?)
 
-    query = Developer.includes(:developer_languages, :languages, :developer_programming_languages, :programming_languages)
+    query = Developer.includes(:languages, :programming_languages)
     query = query.where(languages: { code: language }) if language.present?
     query = query.where(programming_languages: { name: programming_language }) if programming_language.present?
     query = query.order('developers.created_at desc')
